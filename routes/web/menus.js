fcb4b0e355899bec52fd9eb91ccf2e6a0788dc75
Julien <julien.mahecrenn@sky.uk>
2021-11-05 13:13:59 +0000

    Adding functionality for Menu Items

--- routes/web/menus.js
+++ routes/web/menus.js
@@ -25,6 +25,36 @@ Router
             return next(error);
         };
     })
+    .get("/:id/menuitems", async (req,res,next) => {
+        try {
+            const response = await fetch(`${url}/${req.params.id}/menuitems`);
+            const menuitems = await response.json();
+            res.render("menuMenuItems", {menuitems});
+        } catch (error) {
+            return next (error)
+        };
+    })
+    .post("/:id/menuitems", async (req,res,next) => {
+        try {
+            await fetch(`${url}/${req.params.id}/menuitems`, {
+                method: "post",
+                body: JSON.stringify(req.body),
+                headers: { "Content-Type": "application/json"},   
+               });
+            
+            res.redirect(`/menus/${req.params.id}/menuitems`)
+        } catch (error) {
+            return next(error);
+        };
+    })
+    .get("/:id/menuitems/new", async (req,res,next) => {
+        try {
+            const menu_id = req.params.id
+            res.render("newMenuItem", {menu_id} )
+        } catch (error) {
+            return next(error);
+        };
+    })
 //
 
 module.exports = Router;
